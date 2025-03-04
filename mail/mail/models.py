65cd86b51bf76838dbe28db5b5446ef87dc735a1
yamilobt8 <yamilo12345678910@gmail.com>
2025-03-04 13:03:37 +0000

    Project 3 mail

---
+++ mail/mail/models.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+
+class Email(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="emails")
+    sender = models.ForeignKey("User", on_delete=models.PROTECT, related_name="emails_sent")
+    recipients = models.ManyToManyField("User", related_name="emails_received")
+    subject = models.CharField(max_length=255)
+    body = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+    archived = models.BooleanField(default=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "sender": self.sender.email,
+            "recipients": [user.email for user in self.recipients.all()],
+            "subject": self.subject,
+            "body": self.body,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p"),
+            "read": self.read,
+            "archived": self.archived
+        }
+
+""" [
+    {
+        "id": 100,
+        "sender": "foo@example.com",
+        "recipients": ["bar@example.com"],
+        "subject": "Hello!",
+        "body": "Hello, world!",
+        "timestamp": "Jan 2 2020, 12:00 AM",
+        "read": false,
+        "archived": false
+    },
+    {
+        "id": 95,
+        "sender": "baz@example.com",
+        "recipients": ["bar@example.com"],
+        "subject": "Meeting Tomorrow",
+        "body": "What time are we meeting?",
+        "timestamp": "Jan 1 2020, 12:00 AM",
+        "read": true,
+        "archived": false
+    }
+] """
---
+++ mail/mail/static/mail/inbox.js
@@ -0,0 +1,89 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Use buttons to toggle between views
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-form').addEventListener('submit', send_email);
+
+  // By default, load the inbox
+  load_mailbox('inbox');
+});
+
+function compose_email() {
+  console.log('hello 1')
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+}
+
+function load_mailbox(mailbox) {
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  fetch(`/emails/${mailbox}` )
+  .then(response => response.json())
+  .then(emails => {
+      // Print emails
+      console.log(emails);
+
+      const emailsView = document.getElementById("emails-view");
+
+      emailsView.innerHTML = "";
+
+      if (emails.length === 0) {
+        if (mailbox == 'inbox') {
+          emailsView.innerHTML = "<p>No emails In Inbox</p>";
+        }else if{
+          emailsView.innerHTML = `<p>No emails ${mailbox}ed</p>`;
+        }
+        return; // Stop execution
+      }
+
+      emails.forEach(email => {
+        const emailDiv = document.createElement("div");
+        emailDiv.innerHTML = `
+          <strong>From:</strong> ${email.sender} <br>
+          <strong>To:</strong> ${email.recipients.join(", ")} <br>
+          <strong>Subject:</strong> ${email.subject} <br>
+          <strong>Timestamp:</strong> ${email.timestamp} <br>
+          <hr>
+        `;
+        emailsView.appendChild(emailDiv);
+      });
+  })
+  .catch(error => console.error("Error fetching emails:", error));
+}
+
+function send_email(event) {
+  
+  event.preventDefault();
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+  
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+  });
+}
