4fcc96984d51c34b28046d6c96042adf0dbd3619
yamilobt8 <yamilo12345678910@gmail.com>
2025-03-28 02:59:27 +0000

    new url and view function for new post

--- network/network/views.py
+++ network/network/views.py
@@ -3,6 +3,7 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import User
 
@@ -61,3 +62,7 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+@login_required
+def new_post(request):
+    return render(request, 'network/newpost.html')
