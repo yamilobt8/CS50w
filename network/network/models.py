e1a42d68f3ade9e0118e57b515c1ae0d41ec1ac4
yamilobt8 <yamilo12345678910@gmail.com>
2025-04-01 16:04:56 +0000

    Added the followers and following model

--- network/network/models.py
+++ network/network/models.py
@@ -6,6 +6,17 @@ class User(AbstractUser):
     pass
 
 class Post(models.Model):
-    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='posts')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     content = models.TextField(blank=False, null=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+class Follow(models.Model):
+    ACTION_CHOICES = [
+        ("Follow", "Follow"),
+        ("Unfollow", "Unfollow"),
+    ]
+    
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followings')
+    followers = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers') 
+    action = models.CharField(max_length=10, choices=ACTION_CHOICES, default='Follow')
     timestamp = models.DateTimeField(auto_now_add=True)
