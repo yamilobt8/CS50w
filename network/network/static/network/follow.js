0a6515aab34b01a21c2f2b57e7304178d154e19f
yamilobt8 <yamilo12345678910@gmail.com>
2025-04-02 03:22:30 +0000

    add transition to the follow button

--- network/network/static/network/follow.js
+++ network/network/static/network/follow.js
@@ -7,5 +7,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 function follow() {
-
+    const following_btn = document.querySelector('#follow')
+    // change the button theme and content
+    following_btn.textContent = 'Unfollow';
+    following_btn.classList.remove('btn-primary');
+    following_btn.classList.add('btn-outline-success');
+    // if the users follow put a post request to the backend changing the status
 }
